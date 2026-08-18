52d503826a35f91dd830558765193b7a7383c334
Kuzne4ik <sidorov.a@tm.biz-apps.ru>
2026-08-18 19:33:51 +0300

    add lesson 1_1

---
+++ sidorov_lesson1_1/Controllers/StartController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lesson1_1.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Index()
+        {
+            var now = DateTime.Now;
+
+            var nightPeriodStart = new DateTime(now.Year, now.Month, now.Day, 0, 0, 0);
+            var nightPeriodEnd = new DateTime(now.Year, now.Month, now.Day, 5, 59, 59);
+
+            var morningPeriodStart = new DateTime(now.Year, now.Month, now.Day, 6, 0, 0);
+            var morningPeriodEnd = new DateTime(now.Year, now.Month, now.Day, 11, 59, 59);
+
+            var dayPeriodStart = new DateTime(now.Year, now.Month, now.Day, 12, 0, 0);
+            var dayPeriodEnd = new DateTime(now.Year, now.Month, now.Day, 17, 59, 59);
+
+            var eveningPeriodStart = new DateTime(now.Year, now.Month, now.Day, 18, 0, 0);
+            var eveningPeriodEnd = new DateTime(now.Year, now.Month, now.Day, 23, 59, 59);
+
+            if (now >= nightPeriodStart && now <= nightPeriodEnd)
+            {
+                return "Доброй ночи";
+            }
+            else if (now >= morningPeriodStart && now <= morningPeriodEnd)
+            {
+                return "Доброе утро";
+            }
+            else if (now >= dayPeriodStart && now <= dayPeriodEnd)
+            {
+                return "Добрый день";
+            }
+            else if (now >= eveningPeriodStart && now <= eveningPeriodEnd)
+            {
+                return "Добрый вечер";
+            }
+
+
+            return "HI";
+            //return View();
+        }
+    }
+}
