bd6cad8e191385fe83ed692c74453a0d23776b49
Kuzne4ik <psigrasshopper@gmail.com>
2026-08-20 13:00:08 +0300

    Add lesson_1_4

---
+++ sidorov_lesson1_4/Controllers/CalculatorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sidorov_lesson1_4.Controllers
+{
+    [Route("calc")]
+    public class CalculatorController : Controller
+    {
+        [Route("index")]
+        public string Index(double a, double b, string c = "+")
+        {
+            if (c == "+")
+            {
+                var sum = a + b;
+                return a + " " + c + " " + b + " = " + sum;
+            }
+            if (c == "-")
+            {
+                var sum = a - b;
+                return a + " " + c + " " + b + " = " + sum;
+            }
+            if (c == "*")
+            {
+                var sum = a * b;
+                return a + " " + c + " " + b + " = " + sum;
+            }
+            if (c == "/")
+            {
+                var sum = a / b;
+                return a + " " + c + " " + b + " = " + sum;
+            }
+            return $"Неизвестный арифметический оператор: {c}. \nДопустимые значения оператора: +, -, *, /";
+        }
+    }
+}
