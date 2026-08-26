c03e03456a81cd7c4c71ab5b67b043175f6d97dd
Kuzne4ik <psigrasshopper@gmail.com>
2026-08-27 00:18:15 +0300

    A Views/_Layout общий файл для вьюх D Index.html мешает вызову метода index контроллера Home для корреткной проброске View/Home/Index.cshtml по моедли List<Product>

--- sidorov_lesson_2_1_1/Controllers/HomeController.cs
+++ sidorov_lesson_2_1_1/Controllers/HomeController.cs
@@ -1,13 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Routing;
+﻿using lesson2_1.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace lesson_2_1_1.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ProductsRepository _productsRepository;
+        public HomeController()
+        {
+            _productsRepository = new ProductsRepository();
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+
+
+            var products = _productsRepository.GetAll();
+
+            /*
+            // Нет id, выводим все товары
+            foreach (var product in products)
+            {
+                res += $"{product.Id}{Environment.NewLine}{product.Name}{Environment.NewLine}{product.Cost:c}{Environment.NewLine}{Environment.NewLine}";
+            }*/
+
+            return View(products);
         }
     }
 }
